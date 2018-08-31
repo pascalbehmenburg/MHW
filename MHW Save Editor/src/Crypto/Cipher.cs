30e8c46f0a1443aea75d5a05e342484e02e89bdc
Pascal <pascal_fuhrmann@web.de>
2018-08-31 22:17:13 +0200

    Checksum generation, Blowfish integration, Mib & Itlot files supported
    Checksum generation is now working. Blowfish & SHA-1 are now being used instead of the hacky stuff. Itlot and mib files should now work altough it is in an experimental state will be updated soon.

---
+++ MHW Save Editor/src/Crypto/Cipher.cs
@@ -0,0 +1,37 @@
+﻿using BlowFish;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHW
+{
+    class Cipher
+    {
+        private Blowfish blowfish;
+        private readonly string key;
+
+        public Cipher(string key)
+        {
+            this.key = key;
+            this.blowfish = new Blowfish(Encoding.Default.GetBytes(this.key));
+        }
+
+        public byte[] Encipher(byte[] data)
+        {
+            data = Utility.bswap(data);
+            data = blowfish.Encrypt_ECB(data);
+            data = Utility.bswap(data);
+            return data;
+        }
+
+        public byte[] Decipher(byte[] data)
+        {
+            data = Utility.bswap(data);
+            data = blowfish.Decrypt_ECB(data);
+            data = Utility.bswap(data);
+            return data;
+        }
+    }
+}
