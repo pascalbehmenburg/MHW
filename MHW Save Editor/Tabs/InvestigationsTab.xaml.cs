794ff1f45bdffd7491c214dd83d59d158ba769f8
AsteriskAmpersand <aravuegnitsuga@gmail.com>
2018-09-22 16:01:07 -0500

    Forgot a file
    Signed-off-by: AsteriskAmpersand <aravuegnitsuga@gmail.com>

---
+++ MHW Save Editor/Tabs/InvestigationsTab.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MHW_Save_Editor.Tabs
+{
+    public partial class InvestigationsTab : UserControl
+    {
+        public InvestigationsTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
